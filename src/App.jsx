a1c9072d7ccb9b00f445b4c8daa9eb71056fa922
joshpled <joshuapleduc@gmail.com>
2024-03-29 16:31:59 -0400

    working firebase auth on frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import app from './utils/firebaseConfig'
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
+const auth = getAuth(app);
+signInWithEmailAndPassword(auth, 'j@c.com', 'qwerty')
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // You can get the ID token from the user object
+    user.getIdToken().then((idToken) => {
+      // Send the token to your server
+      axios.get('http://localhost:3000/protected-route', {
+        headers: { 'x-token': idToken }
+      })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    });
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode, errorMessage)
+  });
+
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>App</div>
   )
 }
-
-export default App
