339a57d0818b5f05b07277bb4b98380bc948b013
joshpled <joshuapleduc@gmail.com>
2024-04-17 16:30:50 -0400

    change vite config file back

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  server: {
-    historyApiFallback: true,
-  },
   resolve: {
     alias: {
       "@components": "/src/components",
