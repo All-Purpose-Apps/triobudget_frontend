0ad23d4558a55c8747041c107505f2b1ed631c38
joshpled <joshuapleduc@gmail.com>
2024-04-17 16:27:55 -0400

    add server on config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Define the base path of the application
-  base: '/',
+  base: "/",
+  server: {
+    historyApiFallback: true,
+  },
   resolve: {
     alias: {
-      '@components': '/src/components',
-      '@utils': '/src/utils',
-      '@assets': '/src/assets',
-      '@store': '/src/store',
-      '@pages': '/src/pages',
+      "@components": "/src/components",
+      "@utils": "/src/utils",
+      "@assets": "/src/assets",
+      "@store": "/src/store",
+      "@pages": "/src/pages",
     },
   },
 });
